0a45d7a2603d362f57e5d47ce5dbcc3e8028395a
Middletwo-Kid <976989701@qq.com>
2021-01-25 17:05:22 +0800

    feat: 配置css目录和全局样式变量

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `@import "~@/assets/style/var.scss";`
+      }
+    }
+  }
+}
