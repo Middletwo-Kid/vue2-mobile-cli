cd3b57d71de5738e40c887028f81851d12fc6849
Middletwo-Kid <976989701@qq.com>
2021-01-25 15:45:30 +0800

    feat: add git-lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-blocks': 'off',       // 括号前需要空格
+    'trailing comma': 'off',             // 强制加上逗号
+    'class-methods-use-this': 'off',     // 在示例方法中没有使用this，那么ESLint推荐你写成static方法
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
